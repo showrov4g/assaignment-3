a30b89d956df500faa8dff1a1cd1ce6f225f9571
showrov4g <showrovg00@gmail.com>
2024-09-11 23:36:28 +0600

    problem 5 solve

---
+++ problem-5.js
@@ -0,0 +1,16 @@
+function waitingTime(waitingTimes, serialNumber) {
+  if (Array.isArray(waitingTimes) && typeof serialNumber === "number") {
+    let totalTime = 0;
+    for (let num of waitingTimes) {
+      totalTime += num;
+    }
+    let averageTime = totalTime / waitingTimes.length;
+    let roundedAverage = Math.round(averageTime);
+    let whoGiveInterview = Math.max(serialNumber - 1 - waitingTimes.length);
+    let actualWaitingTime = roundedAverage * whoGiveInterview;
+
+    return actualWaitingTime;
+  } else {
+    return "Invalid Input";
+  }
+}
