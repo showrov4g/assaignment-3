c67fc8bdadff25a01dd33d01c94c38bd5de5a26a
showrov4g <showrovg00@gmail.com>
2024-09-11 11:26:14 +0600

    third problem solve;

--- problem-3.js
+++ problem-3.js
@@ -1,6 +1,10 @@
-
 function checkDigitsInName(name) {
-    
+  for (let i = 0; i < name.length; i++) {
+    if (name[i] >= "0" && name[i] <= "9") {
+      return true;
+    } else if (typeof name !== "string") {
+      return "invelid input";
+    }
+  }
+  return false;
 }
-
-
